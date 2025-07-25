477c8cb32b4c2a7bf62890b77bca8d5233fce44d
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-25 16:57:50 -0700

    feat: Add mock Data and Todo type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Data } from "./data/todos"
+
 function App() {
   return (
     <main className="py-10 h-screen">
       <h1 className="font-bold text-3xl text-center">ToDo List</h1>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {Data.map(todo => (
+            <p key={todo.id} className="text-lg">
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../types/todo";
+
+export const Data: Todo[] = [
+    {
+        id: 1,
+        title: "Read for 30 minutes 📚",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Exercise for 1 hour 🏋️",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Go grocery shopping 🛒",
+        completed: false,
+    },
+];
