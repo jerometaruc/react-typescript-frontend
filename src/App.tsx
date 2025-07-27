f39d8d30c1653b2fa7174fd416faa839435032ef
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-26 17:26:57 -0700

    feat: Implement TodoItem completion toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { Data } from "./data/Todos";
 import TodoItem from "./components/todoItem";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(Data);
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map(todo => (
+        todo.id === id ? { ...todo, completed } : todo
+      ))
+    );
+  };
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">ToDo List</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
-          {Data.map(todo => (
-            <TodoItem todo={todo}/>
+          {todos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted}
+            />
           ))}
         </div>
       </div>
