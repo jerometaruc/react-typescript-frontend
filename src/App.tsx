7a4930b44a05fe05f64ef364de2f2f67a3e02b46
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-26 17:07:14 -0700

    feat: Add initial TodoItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { Data } from "./data/Todos"
+import { Data } from "./data/Todos";
+import TodoItem from "./components/todoItem";
 
 function App() {
   return (
-    <main className="py-10 h-screen">
+    <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">ToDo List</h1>
-      <div className="max-w-lg mx-auto">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {Data.map(todo => (
-            <p key={todo.id} className="text-lg">
-              {todo.title}
-            </p>
+            <TodoItem todo={todo}/>
           ))}
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
