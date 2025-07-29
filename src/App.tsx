d7fd453dcc20feacc7970844535500b27274ae03
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-29 15:31:34 -0700

    feat: Add local storage persistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Data } from "./data/Todos";
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
 import TodoSummary from "./components/TodoSummary";
+import type { Todo } from "./types/Todo";
 
 function App() {
-  const [todos, setTodos] = useState(Data);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+    return savedTodos.length > 0 ? savedTodos : Data;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   function setTodoCompleted(id: number, completed: boolean) {
     setTodos((prevTodos) =>
