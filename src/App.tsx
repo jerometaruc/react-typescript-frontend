00a85f61dfca19821708ee8f1536331943e23c4f
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-28 17:30:33 -0700

    feat: Implement AddTodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Data } from "./data/Todos";
-import TodoItem from "./components/todoItem";
 import { useState } from "react";
+import { Data } from "./data/Todos";
+import TodoItem from "./components/TodoItem";
+import AddTodoForm from "./components/AddTodoForm";
 
 function App() {
   const [todos, setTodos] = useState(Data);
@@ -13,10 +14,24 @@ function App() {
     );
   };
 
+  function addTodo(title: string) {
+    setTodos(prevTodos => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ]);
+  };
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">ToDo List</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm
+          onSubmit={addTodo}
+        />
         <div className="space-y-2">
           {todos.map(todo => (
             <TodoItem
