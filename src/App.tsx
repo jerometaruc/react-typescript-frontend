b975758aeffe755f509ab17d141629388b0e601e
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-28 17:57:33 -0700

    feat: Implement TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Data } from "./data/Todos";
-import TodoItem from "./components/TodoItem";
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(Data);
@@ -25,22 +25,22 @@ function App() {
     ]);
   };
 
+  function deleteTodo(id: number) {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  };
+
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">ToDo List</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm
           onSubmit={addTodo}
         />
-        <div className="space-y-2">
-          {todos.map(todo => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-            />
-          ))}
-        </div>
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
       </div>
     </main>
   );
